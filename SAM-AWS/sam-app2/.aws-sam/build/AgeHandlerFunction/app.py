f7dca18cd9409e26dd4a4d1ea1a391fbb9a861ab
Keely001 <thindiuke@gmail.com>
2025-06-10 20:33:20 +0300

    CloudWatch alarm, Cloudwatch logs, and SNS notification for Lambda error.

--- SAM-AWS/sam-app2/.aws-sam/build/AgeHandlerFunction/app.py
+++ SAM-AWS/sam-app2/.aws-sam/build/AgeHandlerFunction/app.py
@@ -9,14 +9,16 @@ sns = boto3.client('sns')
 TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
 
 def lambda_handler(event, context):
-    try:
-        # Parse the request body (from API Gateway)
-        body = json.loads(event['body'])
 
-        # Safely extract values from the JSON, using defaults if missing
-        name = body.get('name', 'User')
-        age = int(body.get('age', 0))
-        gender = body.get('gender', 'Not specified')
+    # Parse the request body (from API Gateway)
+    body = json.loads(event['body'])
+
+    # Safely extract values from the JSON, using defaults if missing
+    name = body.get('name', 'User')
+    age = int(body.get('age', 0))
+    gender = body.get('gender', 'Not specified')
+    
+    try:
 
         # Check if the user is an adult (18+)
         if age >= 18:
@@ -50,8 +52,16 @@ def lambda_handler(event, context):
         }
 
     except Exception as e:
-        # Handle and return any error as HTTP 500 response
+        # Send custom error message to same SNS topic
+        error_message = f"🚨An Itsy bitsy error on the server side occured:\n\nName: {name}\nAge: {age}\nGender: {gender}\n\n\n"
+        sns.publish(
+            TopicArn=TOPIC_ARN,
+            Subject='Server Error',
+            Message=error_message
+        )
+
         return {
             "statusCode": 500,
             "body": json.dumps({"error": str(e)})
         }
+
